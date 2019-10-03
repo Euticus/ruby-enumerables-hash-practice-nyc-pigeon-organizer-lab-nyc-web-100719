3d47610db25cb44ae1056593daa2fffb3c164d8b
Jay  Stewart <jayr.stewart@live.com>
2019-10-03 16:12:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,6 +11,7 @@ def nyc_pigeon_organizer(data)
       if !new_data[name][class_a]
         new_data[name][class_a] = []
       end 
+      if 
       binding.pry
         if class_a = :color 
           new_data[name][:color].push(stats.to_s)
