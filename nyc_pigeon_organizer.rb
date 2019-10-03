1323d4df4d698737a7acc54b6197378189ef208e
Jay  Stewart <jayr.stewart@live.com>
2019-10-03 16:16:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,18 +13,8 @@ def nyc_pigeon_organizer(data)
       end 
       new_data[name][class_a].push(stats)
       binding.pry
-        if class_a = :color 
-          new_data[name][:color].push(stats.to_s)
-        end     
-        if class_a = :gender
-          new_data[name][:gender].push(stats.to_s)
-        end
-        if class_a = :lives
-         new_data[name][:lives].push(stats.to_s)
-        end 
       end
-    end 
+    end
   end
   new_data
-
 end
