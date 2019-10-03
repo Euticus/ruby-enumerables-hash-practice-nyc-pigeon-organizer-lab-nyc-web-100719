ad71939f443d0ed9e926dbb9f8176f0e8953a365
Jay  Stewart <jayr.stewart@live.com>
2019-10-03 15:39:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,11 +16,11 @@ def nyc_pigeon_organizer(data)
          
         end     
         if class_a = :gender
-          new_data[name][:gender].push(stats.key(name))
+          new_data[name][:gender].push(stats.to_s)
            
         end
         if class_a = :lives
-         new_data[name][:lives].push(stats.key(name))
+         new_data[name][:lives].push(stats.to_s)
         end 
         
       end
