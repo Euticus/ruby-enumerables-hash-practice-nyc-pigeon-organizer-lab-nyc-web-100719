f95e3430b9ce6cf36d164c1d4e9429d19d185172
Jay  Stewart <jayr.stewart@live.com>
2019-10-03 15:39:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}          
+  data.each do |class_a, attributes_names|
+    attributes_names.each do |stats, array_names|
+      array_names.each do |name|
+      new_data[name] = {
+        :color => [],
+        :gender => [],
+        :lives => []
+      }
+        if class_a = :color 
+           binding.pry
+          new_data[name][:color].push(stats.key(name))
+         
+        end     
+        if class_a = :gender
+          new_data[name][:gender].push(stats.key(name))
+           
+        end
+        if class_a = :lives
+         new_data[name][:lives].push(stats.key(name))
+        end 
+        
+      end
+          
+    end 
+   
+  end
+  new_data
+
 end
